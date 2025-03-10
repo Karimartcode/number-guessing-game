c39b7fae563812f04a6889a1877805ac1b092fe1
Abdelkarim Mouachiq <abmou87@ulaval.ca>
2025-03-10 17:17:41 +0000

    add hints and game loop

--- main.c
+++ main.c
@@ -6,11 +6,18 @@ int main() {
     srand(time(NULL));
     int target = rand() % 100 + 1;
     int guess;
-    printf("Guess a number between 1 and 100: ");
-    scanf("%d", &guess);
-    if (guess == target)
-        printf("Correct!\n");
-    else
-        printf("Wrong! It was %d\n", target);
+    printf("Guess a number between 1 and 100\n");
+    while (1) {
+        printf("Your guess: ");
+        scanf("%d", &guess);
+        if (guess == target) {
+            printf("Correct!\n");
+            break;
+        } else if (guess < target) {
+            printf("Too low\n");
+        } else {
+            printf("Too high\n");
+        }
+    }
     return 0;
 }
