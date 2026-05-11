013a36bc6384607d5f5bdb84d59dd768aecf6689
Abdelkarim Mouachiq <abmou87@ulaval.ca>
2026-05-11 21:43:49 +0000

    extract play_round function

--- main.c
+++ main.c
@@ -2,23 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-    srand(time(NULL));
-    int target = rand() % 100 + 1;
+int play_round(int max_val) {
+    int target = rand() % max_val + 1;
     int guess, attempts = 0;
-    printf("Guess a number between 1 and 100\n");
+    printf("Guess a number between 1 and %d\n", max_val);
     while (1) {
         printf("Your guess: ");
         scanf("%d", &guess);
         attempts++;
         if (guess == target) {
             printf("Correct! Attempts: %d\n", attempts);
-            break;
+            return attempts;
         } else if (guess < target) {
             printf("Too low\n");
         } else {
             printf("Too high\n");
         }
     }
+}
+
+int main() {
+    srand(time(NULL));
+    play_round(100);
     return 0;
 }
